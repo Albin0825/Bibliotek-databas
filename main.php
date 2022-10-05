ccc87296753d6142a1e17a3ec8f0fa56ae65fac3
Nixeff <theodore.folkesson@gmail.com>
2022-10-05 10:55:04 +0200

    Vissar all media

--- main.php
+++ main.php
@@ -1,3 +1,23 @@
+<?php
+    require "db.php";
+
+    $sql = "SELECT * FROM media";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            if($row["type"] == "Book" || $row["type"] == "Refrense Book"){
+                echo $row["title"] . " | " . $row["type"]. " | " . $row["ageRestriction"]. " | ".$row["length"] ." Pages<br>";
+            } else{
+                echo $row["title"] . " | " . $row["type"]. " | " . $row["ageRestriction"]. " | ".$row["length"] ." Minutes<br>";
+            }
+            
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
