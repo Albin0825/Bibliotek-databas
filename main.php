bb964e05a66887ee0c4217e49ca2af0960293f69
Nixeff <theodore.folkesson@gmail.com>
2022-10-05 11:30:22 +0200

    filter påväg

--- main.php
+++ main.php
@@ -7,8 +7,20 @@
     }else{
         $sql = "SELECT * FROM media";
     }
+    $filter = [];
 
-    
+    if(!empty($_POST["Book"])){
+        array_push($filter,"Book");    
+    }
+    if(!empty($_POST["Audio Book"])){
+        array_push($filter,"Audio Book");
+    }
+    if(!empty($_POST["Refrense Book"])){
+        array_push($filter,"Refrense Book");
+    }
+    if(!empty($_POST["Movie"])){
+        array_push($filter,"Movie");
+    }
 
     $result = $conn->query($sql);
 
@@ -26,20 +38,32 @@
 <body>
 
     <div>
-        <form method="POST">
-            <input type="text" name="search"/><input type="submit" value="Search"/>
+        <form id="searchf" method="POST" style="display:flex">
+            <input type="text" name="search" autofocus/>
+            <p> | Book</p>
+            <input type="checkbox" name="Book" checked/>
+            <p> | Audio Book</p>
+            <input type="checkbox" name="Audio Book" checked/>
+            <p> | Refrense Book</p>
+            <input type="checkbox" name="Refrense Book" checked/>
+            <p> | Moive</p>
+            <input type="checkbox" name="Movie" checked/>
+            <input type="submit" value="Search"/>
         </form>
         <?php
 
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
-                if($row["type"] == "Book" || $row["type"] == "Refrense Book"){
-                    echo $row["title"] . " | " . $row["type"]. " | " . $row["ageRestriction"]. " | ".$row["length"] ." Pages<br>";
-                } else{
-                    echo $row["title"] . " | " . $row["type"]. " | " . $row["ageRestriction"]. " | ".$row["length"] ." Minutes<br>";
+                for($i = count($filter)-1;$i>0;--$i){
+                    if ($filter[$i] == $row["type"]){
+                        if($row["type"] == "Book" || $row["type"] == "Refrense Book"){
+                            echo $row["title"] . " | " . $row["type"]. " | " . $row["ageRestriction"]. " | ".$row["length"] ." Pages<br>";
+                        } else{
+                            echo $row["title"] . " | " . $row["type"]. " | " . $row["ageRestriction"]. " | ".$row["length"] ." Minutes<br>";
+                        }
+                    }
                 }
-                
             }
         }
         ?>
