2283c7d73704d4e64d6c0a8ddf3e5f00793dc644
Nixeff <theodore.folkesson@gmail.com>
2022-10-05 13:24:43 +0200

    Filter funkar wooooooooooooooooh

--- main.php
+++ main.php
@@ -12,10 +12,10 @@
     if(!empty($_POST["Book"])){
         array_push($filter,"Book");    
     }
-    if(!empty($_POST["Audio Book"])){
+    if(!empty($_POST["AudioBook"])){
         array_push($filter,"Audio Book");
     }
-    if(!empty($_POST["Refrense Book"])){
+    if(!empty($_POST["RefrenseBook"])){
         array_push($filter,"Refrense Book");
     }
     if(!empty($_POST["Movie"])){
@@ -41,31 +41,40 @@
         <form id="searchf" method="POST" style="display:flex">
             <input type="text" name="search" autofocus/>
             <p> | Book</p>
-            <input type="checkbox" name="Book" checked/>
+            <input type="checkbox" name="Book" checked></input>
             <p> | Audio Book</p>
-            <input type="checkbox" name="Audio Book" checked/>
+            <input type="checkbox" name="AudioBook" checked></input>
             <p> | Refrense Book</p>
-            <input type="checkbox" name="Refrense Book" checked/>
+            <input type="checkbox" name="RefrenseBook" checked></input>
             <p> | Moive</p>
-            <input type="checkbox" name="Movie" checked/>
-            <input type="submit" value="Search"/>
+            <input type="checkbox" name="Movie" checked></input>
+            <input type="submit" value="Search"></input>
         </form>
         <?php
 
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
-                for($i = count($filter)-1;$i>0;--$i){
-                    if ($filter[$i] == $row["type"]){
+                if(!empty($filter)){
+                    foreach($filter as $type){
+                        if ($type == $row["type"]){
+                            if($row["type"] == "Book" || $row["type"] == "Refrense Book"){
+                                echo $row["title"] . " | " . $row["type"]. " | " . $row["ageRestriction"]. "+ | ".$row["length"] ." Pages<br>";
+                            } else{
+                                echo $row["title"] . " | " . $row["type"]. " | " . $row["ageRestriction"]. "+ | ".$row["length"] ." Minutes<br>";
+                            }
+                        }
+                    }
+                } else{
                         if($row["type"] == "Book" || $row["type"] == "Refrense Book"){
-                            echo $row["title"] . " | " . $row["type"]. " | " . $row["ageRestriction"]. " | ".$row["length"] ." Pages<br>";
+                            echo $row["title"] . " | " . $row["type"]. " | " . $row["ageRestriction"]. "+ | ".$row["length"] ." Pages<br>";
                         } else{
-                            echo $row["title"] . " | " . $row["type"]. " | " . $row["ageRestriction"]. " | ".$row["length"] ." Minutes<br>";
+                            echo $row["title"] . " | " . $row["type"]. " | " . $row["ageRestriction"]. "+ | ".$row["length"] ." Minutes<br>";
                         }
                     }
                 }
+
             }
-        }
         ?>
     </div>
 </body>
