6457a8e2b4af7ae569e08ee8145f3f308c246a77
Nixeff <theodore.folkesson@gmail.com>
2022-10-06 15:45:40 +0200

    fan

--- main.php
+++ main.php
@@ -45,17 +45,31 @@
         array_push($filter,"Movie");
     }
 /*-----------------------------------------------------------
-            Reserve
+            Reserved
 -----------------------------------------------------------*/
     if (!empty($_POST['reserve'])){
         $reserve = $_POST['reserve'];
-        $sql = "SELECT * FROM queue";
+        $sql = "INSERT INTO queue (uID, mID, qDate) VALUE ($uID,$reserve,'$date')";
         $reserveRes = $conn->query($sql); 
         array_push($filter,"Book");
         array_push($filter,"Audio Book");
         array_push($filter,"Refrense Book");
         array_push($filter,"Movie");
     }
+
+/*-----------------------------------------------------------
+            unReserve
+-----------------------------------------------------------*/
+    if (!empty($_POST["unReserve"])){
+        $unReserve = $_POST["unReserve"];
+        $sql = "DELETE FROM queue WHERE queue.mID = ".$unReserve;
+        $retrunRet = $conn->query($sql);
+        array_push($filter,"Book");
+        array_push($filter,"Audio Book");
+        array_push($filter,"Refrense Book");
+        array_push($filter,"Movie");
+    }
+
 
 /*-----------------------------------------------------------
             Filter
@@ -93,6 +107,22 @@
             array_push($borrowed,$row);
         }
     }
+
+/*-----------------------------------------------------------
+            Reserve
+-----------------------------------------------------------*/
+$sql = "SELECT * FROM queue";
+
+$resultborrow = $conn->query($sql);
+
+$queue = [];
+if ($resultborrow->num_rows > 0) {
+    // output data of each row
+    while($row = $resultborrow->fetch_assoc()) {
+        array_push($queue,$row);
+    }
+}
+
 
 
 
@@ -146,18 +176,18 @@
                 $temp = 0;
                 foreach($filter as $type){
                     if ($type == $row["type"]){
-                        foreach($borrowed as $b){
-                            if($b['mID'] == $row["ID"] && $b['uID'] == $uID){
-                                echo"<input type='hidden' name='return' value='$mID'/> <input type='submit' value='Return'/></form> <br>";
-                                $temp = 1;
-                            } else if ($b['mID'] == $row["ID"]){ 
-                                echo"<input type='hidden' name='reserve' value='$mID'/> <input type='submit' value='Reserve'/></form> <br>";
-                                $temp = 1;
-                            }    
-                        }
-                        if($temp == 0){
-                            echo"<input type='hidden' name='media' value='$mID'/> <input type='submit' value='Borrow'/></form> <br>";
-                        }
+                            foreach($borrowed as $b){
+                                if($b['mID'] == $row["ID"] && $b['uID'] == $uID && $type != "Refrense Book"){
+                                    echo"<input type='hidden' name='return' value='$mID'/> <input type='submit' value='Return'/></form> <br>";
+                                    $temp = 1;
+                                } else if ($b['mID'] == $row["ID"] && $type != "Refrense Book"){ 
+                                    echo"<input type='hidden' name='reserve' value='$mID'/> <input type='submit' value='Reserve'/></form> <br>";
+                                    $temp = 1;
+                                }
+                            }
+                            if($temp == 0){
+                                echo"<input type='hidden' name='media' value='$mID'/> <input type='submit' value='Borrow'/></form> <br>";
+                            }
                     }
                 }
             }}
